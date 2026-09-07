06ca699d564ec4d3a4dd8fcd6dfd7c132da2092e
sheetholidays <50959169+Sheet4274@users.noreply.github.com>
2026-09-07 22:51:14 +0530

    server.js

--- server.js
+++ server.js
@@ -4,23 +4,25 @@ const fetch = require('node-fetch');
 const app = express();
 app.use(express.json());
 
-// Free Alternative API Route
+// Booking.com Map Properties List Route
 app.get('/api/booking/cities', async (req, res) => {
     try {
-        const response = await fetch('https://booking-com.p.rapidapi.com/v1/hotels/locations?name=Paris&locale=en-gb', {
+        const response = await fetch('https://apidojo-booking-v1.p.rapidapi.com/properties/list-by-map?room_qty=1&guest_qty=1&bbox=14.291283%2C14.948423%2C120.755688%2C121.136864&search_id=none&children_age=11%2C5&price_filter_currencycode=USD&categories_filter=class%3A%3A1%2Cclass%3A%3A2%2Cclass%3A%3A3&languagecode=en-us&travel_purpose=leisure&children_qty=2&order_by=popularity&offset=0', {
             method: 'GET',
             headers: {
-                'x-rapidapi-host': 'booking-com.p.rapidapi.com',
+                'Content-Type': 'application/json',
+                'x-rapidapi-host': 'apidojo-booking-v1.p.rapidapi.com',
                 'x-rapidapi-key': '54b0132b50msh88120a0c3aec79ep1cfcefjsn69cc3e68ee1f'
             }
         });
         const data = await response.json();
         res.json(data);
     } catch (error) {
-        res.status(500).json({ error: 'Failed to fetch data' });
+        res.status(500).json({ error: 'Failed to fetch properties' });
     }
 });
 
+// Root Route
 app.get('/', (req, res) => {
     res.send('Server Active');
 });
